646700ad712c6402cd66e15fa9425c1bcd605578
Hemant Pandey <hemant.pandey@nutanix.com>
2023-01-21 14:30:13 +0530

    Add header with logo and button

---
+++ src/Header.svelte
@@ -0,0 +1,36 @@
+<header>
+    <div
+        class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center"
+    >
+        <a
+            class="flex title-font font-medium items-center text-white mb-4 md:mb-0"
+        >
+            <!-- <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+                viewBox="0 0 24 24"
+            >
+                <path
+                    d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"
+                />
+            </svg> -->
+
+            <span class="ml-3 text-xl">🦈 sharkbite</span>
+        </a>
+        <nav
+            class="md:ml-auto flex flex-wrap items-center text-base justify-center"
+        >
+            <!-- <a class="mr-5 hover:text-white">First Link</a>
+            <a class="mr-5 hover:text-white">Second Link</a> -->
+        </nav>
+        <button
+            class="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+            >Login
+        </button>
+    </div>
+</header>
--- src/MainPage.svelte
+++ src/MainPage.svelte
@@ -4,10 +4,13 @@
     import MainButton from "./MainButton.svelte";
     import WaveFooter from "./WaveFooter.svelte";
     import { responses } from "./stores";
+    import Header from "./Header.svelte";
+
     responses.subscribe((q) => console.log(q));
 </script>
 
-<div class="text-gray-400 body-font">
+<div class="text-gray-400 body-font font-mono">
+    <Header />
     <div class="container px-5 py-24 mx-auto flex flex-col lg:w-4/6 space-y-10">
         <!-- <h1>You</h1> -->
         <TextArea />
